6bd0d61d1a07013a8f64576ad6458b9ef4bd6b99
Sonia Poltoratski <soniapolt@gmail.com>
2018-10-29 17:44:18 -0700

    backwards experiment compatabiltiy added

--- fmri/vpnlROI.m
+++ fmri/vpnlROI.m
@@ -1,9 +1,21 @@
-function thisROI = vpnlROI(thisROI,subj)
+function thisROI = vpnlROI(thisROI,subj,expt)
 
 % commonly used subjects and ROI specifications
+if ~exist('expt','var') % defaults
 initials = {'JG'            'SP'                'EM'            'AS'        'TH'            'AR'            'MN'};
 ret = {     'wfCSS_f_'      'toonRet_f_'        'toonRet_f_'    'wfCSS_f_'  'toonRet_f_'    'toonRet_f_'    'toonRet_f_'};
 face = {    'f_'            'latprf_f_'         'latprf_f_'     'f_'        'fLoc_f_'       'fLoc_f_'       'fLoc_f_'};
+
+else
+    switch expt
+        case 'invPRF3'
+        initials = {'JG'            'SP'        'MG'            'DF'        };
+        ret = {     'wfCSS_f_'      'f_'        'wfCSS_f_'      'wfCSS_f_'  };
+        face = {    'f_'            'f_'        'f_'            'f_'        };
+   
+    end
+end
+
 
 try
     ret = ret{ismember(initials,subj)};
@@ -17,13 +29,5 @@ if containsTxt(thisROI,'faces')
     thisROI = [face thisROI];
 else thisROI = [ret thisROI]; end
 
-% if exist('subj','var') && strcmp(subj,'EM')
-%  thisROI = ['latprf_f_' thisROI];       
-% elseif strcmp(thisROI(end-4:end),'faces')==1 || [exist('subj','var') && strcmp(subj,'SP')]
-%     thisROI = ['f_' thisROI];
-% else  thisROI = ['wfCSS_f_' thisROI];
-% end
-% if avgSession ==1
-%     thisROI = ['avg_' thisROI]; end
 end
 
