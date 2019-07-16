bb55c5053e2ae3acd8c6a8a811b14ba17fcc6112
Sonia Poltoratski <soniapolt@gmail.com>
2019-07-15 19:25:48 -0700

    log behavioral sessions

--- expts/addToLog.m
+++ expts/addToLog.m
@@ -1,22 +1,23 @@
-%function addToLog(initals,subjNum,age,gender,time,logName)
+function addToLog(logName,initials,subjNum,age,gender,time)
 % function addToLog(demo,logName)
 % this function will check for the existance of a log file and either load
 % or create it; if it exists, this subject is appended to the log
 %  input: demo should contain fields: name (initials), age, gender, and
 %  time
-logName = 'testLog.mat';
+% logName = 'testLog.mat';
 
 if exist(logName,'file') load(logName); 
-    log(end+1).name = intials;
-    log(end+1).num = subjNum;
-    log(end+1).age = age;
-    log(end+1).gender = gender;
-    log(end+1).date = date;
+    session(end+1).name = initials;
+    session(end+1).num = subjNum;
+    session(end+1).age = age;
+    session(end+1).gender = gender;
+    session(end+1).date = time;
 else 
-    log = struct('name',intials,'subjNum',subjNum,'age',age,'gender',gender,'date',date);
+    session = struct('name',initials,'subjNum',subjNum,'age',age,'gender',gender,'date',date);
 end
 
-save(logName,log);
+save(logName,'session');
+fprintf('Added subject %s (session #%d) to log: %s.\n',initials,length(session),logName);
 
 
 %end
