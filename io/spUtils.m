7cb825bf556bbf3fcc99ccca06e7a1ca3b6ff181
Sonia Poltoratski <soniapolt@gmail.com>
2019-07-18 18:10:10 -0700

    more robust

--- io/spUtils.m
+++ io/spUtils.m
@@ -5,15 +5,10 @@ tryDirs = { '/Volumes/projects/sonia/utils',...
     '/share/kalanit/projects/sonia/utils',...
     '/Users/kalanit/Experiments/Sonia/utils',...
     '/Users/Sonia/matlab/utils'};
+try
+utilsDir = tryDirs{find(isfolder(tryDirs),1,'first')};
+catch error('Cannot find sp-utils!'); end
 
-found = 0; while ~found
-    for n = 1:length(tryDirs)
-        if isdir(tryDirs{n})
-            utilsDir = tryDirs{n}; found = 1; end
-        if n==length(tryDirs) && ~found
-            error('Cannot find SPUTILS...'); end
-    end % try in sequential order but go with first found
-end
 end
 
 
