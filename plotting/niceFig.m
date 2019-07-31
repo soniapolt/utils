dedeff4f879b8ecf2f2ef25e1eab2cbb4652c178
Sonia Poltoratski <soniapolt@gmail.com>
2019-07-30 23:30:08 -0700

    adding some default values

--- plotting/niceFig.m
+++ plotting/niceFig.m
@@ -1,5 +1,8 @@
 function f = niceFig(figSize,fontSize,painters)
 % initialized figure with some nicer params
+if ~exist('figSize','var') figSize = [.1 .1 .8 .8]; end
+if ~exist('fontSize','var') fontSize = 12; end
+
 f= figure;set(gcf,'color',[1 1 1],'Units', 'Normalized', 'OuterPosition', figSize,'DefaultTextFontSize',fontSize);
 
 if exist('painters','var')
