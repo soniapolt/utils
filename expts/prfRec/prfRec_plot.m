782f2298c18d1a612021b8a58a00ee2640bf85dc
Sonia Poltoratski <soniapolt@gmail.com>
2019-08-16 13:37:45 -0700

    update to prfRec

--- expts/prfRec/prfRec_plot.m
+++ expts/prfRec/prfRec_plot.m
@@ -2,7 +2,7 @@ function prfRec_plot(metric,metricName,labels,order)
 % function plotPerf(perf)
 % simple session performance barplot
 if ~exist('labels','var')
-labels = {'LowerLeft' 'Center' 'UpperRight'}; end
+labels = {'Low' 'Center' 'Up'}; end
 if ~exist('order','var')
 order = [5 4 6 2 1 3]; end
 
