bf5e73fc0faa62bffab6e61af64f3449b808a483
Sonia Poltoratski <soniapolt@gmail.com>
2019-08-23 12:33:21 -0700

    flip mask template model init

--- cssFit/cssFit_sherlock.m
+++ cssFit/cssFit_sherlock.m
@@ -53,12 +53,19 @@ switch whichCSS
         [fits.parNames] = deal({'Y','X','sd','gain','exp'});
         [modelfun, model, metric, resampling] = init_kayCSS(stimSize,hem,im.ppd);
     
+    case 'flipCSSn'
+        expN = .2;
+        modelName = ['flip CSS + fixed exponent: ' num2str(expN)];
+        [fits.parNames] = deal({'Y','X','sd','gain','wNose','wMouth','wSkin','wHair','wEyes'});
+        [fits.expN] = deal(expN);
+        [modelfun1, model1, metric1, resampling1] = init_flipCSSn(expN,stimSize,hem,im.ppd,1);
+        [modelfun, model, metric, resampling] = init_flipCSSn(expN,stimSize,hem,im.ppd,0);
     case 'tempCSSn'
         expN = .2;
         modelName = ['template CSS + fixed exponent: ' num2str(expN)];
         [fits.parNames] = deal({'Y','X','sd','gain','wNose','wMouth','wSkin','wHair','wEyes'});
         [fits.expN] = deal(expN);
-        [modelfun, model, metric, resampling] = init_tempCSSn(expN,stimSize,hem,im.ppd);
+        [modelfun, model, metric, resampling] = init_tempCSSn(expN,stimSize,hem,im.ppd);    
     case 'tempCSS'            
         modelName = 'template CSS model';
         [fits.parNames] = deal({'Y','X','sd','gain','exp','wNose','wMouth','wSkin','wHair','wEyes'});
@@ -91,6 +98,17 @@ for c = 1:length(fits)
                 'optimoptions', {{'Display' 'off' 'UseParallel' 0}}...
                 );
             
+        elseif strcmp('flipCSSn',whichCSS) && strcmp(fits(1).cond,'Inverted')
+            % model1 has masks inverted
+            opt = struct( ...
+                'stimulus',    stimulus{c}, ...
+                'data',        fits(c).vox(v).betas, ...
+                'model',       {model1}, ...
+                'resampling',  resampling1, ...
+                'metric',      metric1, ...
+                'dosave',      'modelfit',...
+                'optimoptions', {{'Display' 'off' 'UseParallel' 0}}...
+                );      
         else
             opt = struct( ...
                 'stimulus',    stimulus{c}, ...
