b84b2d8d2f9e32e3f9515ca04b8d8ed2c994e5fa
Sonia Poltoratski <soniapolt@gmail.com>
2019-12-04 18:00:03 -0800

    deletions

--- io/containsTxt.m
+++
@@ -1,9 +0,0 @@
-function [r] = containsTxt(longStr, findStr)
-% string comparison 
-if ~isempty(strfind(longStr,findStr))
- r = 1;
-else r = 0;
-end
-
-end
-
--- io/weightIms.m
+++
@@ -1,6 +0,0 @@
-function [weightedIms] = weightIms(imMatrix,weightVector)
-% function [scaledIms] = scaleIms(imMatrix,scaleVector)
-% scaling image matrix (nxmxp) by a vector of weights (1xp)
-weightedIms = bsxfun(@times,imMatrix,reshape(weightVector,1,1,length(weightVector)));
-end
-
