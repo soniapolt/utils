53709f28387b0aa4a7f25c32e1981dca572b13ce
Sonia Poltoratski <soniapolt@gmail.com>
2020-01-06 12:07:52 -0800

    no more ptd deo more ptb dependency

--- io/onLaptop.m
+++ io/onLaptop.m
@@ -3,14 +3,16 @@ function [lap] = onLaptop
 % desktop monitor. useful for plotting figures.
 % also checks to see if you're connected to an external monitor of any kind
 
-scr = get(0,'screensize');
-if scr(3)<1793
-    lap = 1;
-else lap = 0;
-end
+if isdir('/share/kalanit') lap = 0; else lap = 1; end
 
-try
-if length(Screen('Screens')) > 1 lap = 0; end
-catch warning(sprintf('Missing PTB...\n')); end
-end
+% scr = get(0,'screensize');
+% if scr(3)<1793
+%     lap = 1;
+% else lap = 0;
+% end
+% 
+% try
+% if length(Screen('Screens')) > 1 lap = 0; end
+% catch warning(sprintf('Missing PTB...\n')); end
+% end
 
