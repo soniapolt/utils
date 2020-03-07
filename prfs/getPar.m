43615f17c98e49aac431427e323a85ab3de72330
Sonia Poltoratski <soniapolt@gmail.com>
2020-03-06 16:00:58 -0800

    fixing X re-centering bug

--- prfs/getPar.m
+++ prfs/getPar.m
@@ -3,6 +3,8 @@ function [pars] = getPar(parName,fits,dva)
 % function to deal with extracting parameters, in raw or prfSet form, from
 % our structs flexibly
 
+if ~isfield(fits,'parNames') fits.parNames = {'Y' 'X' 'sd' 'gain' 'exp'}; end
+
 parNum = cellNum(parName,fits.parNames);
 
 if ~isempty(parNum)
@@ -16,15 +18,14 @@ else
 if isempty(pars)
     error(sprintf('Could not grab pars %s from struct!\n',parName));end
 
-
-if dva && containsTxt(parName,'Y') || containsTxt(parName,'X') || containsTxt(parName,'sd')
-    % rescale some parameters so that they are in DVA units and
-    % centered around zero (center of screen)
-    if ~containsTxt(parName,'sd') % don't re-center the SD
-        pars = fits.res-pars-fits.res/2;
-    end
-    pars = pars./fits.ppd;
+% rescale some parameters so that they are in DVA units and
+% centered around zero (center of screen)
+if dva && containsTxt(parName,'Y')  % don't re-center the SD
+    pars = -(pars-fits.res/2)/fits.ppd;
+elseif dva && containsTxt(parName,'X')
+    pars = (pars-fits.res/2)/fits.ppd;
+elseif dva && containsTxt(parName,'sd')
+    pars = pars/fits.ppd;
 end
 
 end
-
