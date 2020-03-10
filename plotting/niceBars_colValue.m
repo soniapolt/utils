ed1a8e243a7f54ee173ad026e72aac1254a88930
Sonia Poltoratski <soniapolt@gmail.com>
2020-03-10 16:56:49 -0700

    barplot where each bar is colored according to cmap

---
+++ plotting/niceBars_colValue.m
@@ -0,0 +1,23 @@
+function [hbar] = niceBars_colValue(x,data,errors,cmin,cmax,cmap)
+% function [hbar] = niceBars_colValue(x,data,errors)
+% color bars by their value - for now, only a brightened jet colormap to
+% correspond to coverage maps for pRFs
+% features to add: errorbar color handling.
+% SP 3/9/20
+
+if ~exist('cmin','var') cmin = 0; end
+if ~exist('cmax','var') cmax = 1; end
+if ~exist('cmap','var'); cmap = brighten(colormap('jet'),.5); end
+
+colors = cmaplookup(data,cmin,cmax,0,cmap);
+
+hbar = bar(x, data,.33,'FaceColor','flat'); hold on;
+hbar.CData = colors;
+hbar.EdgeColor = 'none';
+h = errorbar(x, data,errors,'k','linestyle', 'none');% Plot with errorbars
+h.CapSize = 0; 
+
+
+colormap(cmap);colorbar;caxis([cmin cmax]);
+end
+
