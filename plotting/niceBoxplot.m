b532819beb738b2e67f1851dc87a50e32a0d60e7
Sonia Poltoratski <soniapolt@gmail.com>
2020-03-10 17:01:02 -0700

    adding individua individual subject data points

--- plotting/niceBoxplot.m
+++ plotting/niceBoxplot.m
@@ -1,4 +1,4 @@
-function b = niceBoxplot(data,labels,plotMed,colors,cutY)
+function b = niceBoxplot(data,labels,plotMed,colors,cutY,plotData)
 % colors needs to be in n-by-3 matrix
 % plotMed will label the medians
 % cutY sets a ylimit
@@ -6,17 +6,29 @@ axis square;
 
 if ~exist('colors','var')
     colors = [condColors(1); condColors(2)]; end
+if ~exist('plotData','var') plotData = 0; end
 
+b = boxplot(data,'labels',labels,'plotstyle','traditional','outliersize',.5,'jitter',.1,'widths',.3);%,'ColorGroup',colors);
+c = findobj(gca,'tag','Median'); d = findobj(gca,'tag','Box');
+e = findobj(gca,'tag','Upper Whisker'); f = findobj(gca,'tag','Lower Whisker'); 
 
-b = boxplot(data,'labels',labels,'plotstyle','traditional','outliersize',.5,'jitter',.1);%,'ColorGroup',colors);
-c = findobj(gca,'tag','Median'); b = findobj(gca,'tag','Box');
-for x = 1:length(c) set(c(x),'Color',colors(x,:)); set(b(x),'Color',colors(x,:)); end
+for x = 1:length(c) 
+    set(c(x),'Color',colors(x,:)); set(d(x),'Color',colors(x,:)); 
+    set(e(x),'LineStyle','-','Color',colors(x,:)*.5); 
+    set(f(x),'LineStyle','-','Color',colors(x,:)*.5); 
+end
+
+delete(findobj(gca,'Tag','Upper Adjacent Value'));delete(findobj(gca,'Tag','Lower Adjacent Value'));
+
+if plotData 
+   hold on; plot(1:size(data,2),data,'k.')
+end
 
 h = findobj(gca,'tag','Outliers');
 set(h,'MarkerEdgeColor',[.5 .5 .5]);
 
 if exist('cutY','var') && ~isempty(cutY)
-    ylim(cutY); end
+    ylim([cutY]); end
 
 
 set(gca,'box','off','color','none');
