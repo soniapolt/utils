3553af684e64eaa14dce6d367a803854567b3b8c
Sonia Poltoratski <soniapolt@gmail.com>
2020-03-10 17:04:15 -0700

    added binary/ded binary/density option

--- prfs/bootCoverage.m
+++ prfs/bootCoverage.m
@@ -8,7 +8,7 @@ boot.propVox = propVox;
 boot.numVox = round(propVox * length(vox));
 boot.ppd = 10;
 boot.res = 11 * boot.ppd;
-boot.method = method; % 'max' or 'mean'
+boot.method = method; % 'max' or 'mean' or 'binary'
 boot.rescale = doNorm; % rescale to 1 to 0
 
 [X,Y]=meshgrid(-boot.res/2:boot.res/2,-boot.res/2:boot.res/2);
@@ -16,7 +16,18 @@ boot.rescale = doNorm; % rescale to 1 to 0
 % make pRFs for all vox
 
 for v = 1:length(vox)
+    if ~strcmp(boot.method,'binary')
     allPRFs(v,:,:) = PRF(X,Y,vox(v).XYdeg(1)*boot.ppd,vox(v).XYdeg(2)*boot.ppd,vox(v).size*boot.ppd);
+    else
+        allPRFs(v,:,:) = (sqrt((X - vox(v).XYdeg(1)*boot.ppd).^2 + (Y - vox(v).XYdeg(2)*boot.ppd).^2) <= vox(v).size*boot.ppd);
+    end   
+        % sanity checking that these methods are the same:
+%         a = PRF(X,Y,vox(v).XYdeg(1)*boot.ppd,vox(v).XYdeg(2)*boot.ppd,vox(v).size*boot.ppd);
+%         b = (sqrt((X - vox(v).XYdeg(1)*boot.ppd).^2 + (Y - vox(v).XYdeg(2)*boot.ppd).^2) <= vox(v).size*boot.ppd);
+%         figure; title(num2str(v)); subplot(1,3,1); imshow(a); subplot(1,3,2); imshow(b); 
+%         C = imfuse(a,b,'falsecolor','Scaling','joint','ColorChannels',[1 2 0]);
+%         subplot(1,3,3); imshow(C);
+    
 end
 
 covIm = zeros(boot.numIters,size(allPRFs,2),size(allPRFs,3));
