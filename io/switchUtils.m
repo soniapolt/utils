e2fe55934f26332a6051c340303a995362febf61
Sonia Poltoratski <soniapolt@gmail.com>
2020-04-09 16:01:57 -0700

    new MBP filepath

--- io/switchUtils.m
+++ io/switchUtils.m
@@ -3,6 +3,6 @@ function switchUtils
 % removes path to remote utils, adds path to local utils - can help speed
 % things up if you're working remotely
 rmpath(genpath('/Volumes/projects/sonia/utils'));
-addpath(genpath('/Users/sonia/matlab/utils'));
+addpath(genpath('/Users/sonia/Documents/MATLAB/utils'));
 end
 
