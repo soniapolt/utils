8bc7d86021d1955e19d8df698a0345557d00f2b7
Sonia Poltoratski <soniapolt@gmail.com>
2020-04-22 16:42:00 -0700

    option to select subset of file name, eg subject initials

--- io/fileName.m
+++ io/fileName.m
@@ -1,7 +1,11 @@
-function [file,pathN] = fileName(path)
+function [file,pathN] = fileName(path,nChars)
+% [file,pathN] = fileName(path,nChars)
 % simple(r) return of filename from path
 
 [pathN,file] = fileparts(path);
+
+if exist('nChars','var')
+    file = file(1:nChars); end
 
 end
 
