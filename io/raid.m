5bf98b2ee30af4fd9eea239cdd230cf6018d1d2e
Sonia Poltoratski <soniapolt@gmail.com>
2021-03-03 16:25:18 -0800

    flexibly allowing Volumes-6 mount

--- io/raid.m
+++ io/raid.m
@@ -1,6 +1,10 @@
 function raidDir = raid
-if onLaptop raidDir = '/Volumes/projects/';
+if onLaptop
+    if isdir('/Volumes/projects-6/')
+        raidDir = '/Volumes/projects-6/';
+    else
+        raidDir = '/Volumes/projects/';
+    end
 else
-raidDir = '/share/kalanit/biac2/kgs/projects/'; end
+    raidDir = '/share/kalanit/biac2/kgs/projects/'; end
 end
-
