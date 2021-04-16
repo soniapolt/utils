49d5c7d64e8d8329e45e2b0654a5427cab60748b
Sonia Poltoratski <soniapolt@gmail.com>
2021-04-15 18:47:00 -0700

    plain text size definitions

--- plotting/niceFig.m
+++ plotting/niceFig.m
@@ -3,6 +3,17 @@ function f = niceFig(figSize,fontSize,painters)
 if ~exist('figSize','var') figSize = [.1 .1 .8 .8]; end
 if ~exist('fontSize','var') fontSize = 12; end
 
+if ~isnumeric(figSize)
+    switch figSize
+        case 'square'
+            figSize = [.1 .1 .5 .8];
+        case 'wide'
+            figSize = [.1 .1 .8 .4];
+        case 'tall'
+            figSize = [.1 .1 .3 .9];
+    end
+end
+
 f= figure;set(gcf,'color',[1 1 1],'Units', 'Normalized', 'OuterPosition', figSize,'DefaultTextFontSize',fontSize);
 
 if exist('painters','var')
